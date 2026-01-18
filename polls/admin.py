7f4702dc5e0ecae2a70baa503b008ec225ce71fc
Darshan-Dalsaniya <dalsaniyadarshan122@gmail.com>
2026-01-18 19:41:29 +0530

    first commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Question
+
+# This tells Django that Question objects have an admin interface
+admin.site.register(Question)
